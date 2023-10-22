4e92b9e037a27bf9f594a0dec7ad6b9abda4f6d4
techmehedi <ecommehedi@gmail.com>
2023-10-21 20:42:53 -0400

    test project

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('myButton').addEventListener('click', function() {
+        window.location.href = 'myuni.html';
+    });
+    document.getElementById('myRes').addEventListener('click', function() {
+        window.location.href = 'myresume.html';
+    })
+    document.getElementById('gohome').addEventListener('click', function() {
+        window.location.href = 'index.html';
+    })
+});
